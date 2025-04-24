eeb7bfa0005f0f1ff1fdff2f825bc50d9e115c87
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-04-24 13:16:52 +0200

    #2 - The page 'guide' is added to the router

--- Web_App/src/router/index.js
+++ Web_App/src/router/index.js
@@ -1,33 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/controlview',
+      path: "/controlview",
       name: "control",
-      component: () => import('../views/controlview.vue')
+      component: () => import("../views/controlview.vue"),
     },
     {
-      path: '/statisticsview',
+      path: "/statisticsview",
       name: "statistics",
-      component: () => import('../views/statisticsview.vue')
-    }
+      component: () => import("../views/statisticsview.vue"),
+    },
+    {
+      path: "/guideview",
+      name: "guide",
+      component: () => import("../views/guideview.vue"),
+    },
   ],
-})
+});
 
-export default router
+export default router;
