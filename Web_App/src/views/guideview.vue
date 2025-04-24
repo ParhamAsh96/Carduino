ba2adb14825072e9baf2dfea1a7aff87962e611c
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-04-24 12:10:44 +0200

    #2 - The page 'guide' is added

---
+++ Web_App/src/views/guideview.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text">this is where the statistics are</h1>
+</template>
+
+<style>
+.text {
+  font-size: 20px; /* Adjust the size of the text */
+  color: red;
+  text-align: center;
+  margin-bottom: 20px; /* Adjust the margin to fit text above the bottom */
+}
+</style>
+<script></script>
