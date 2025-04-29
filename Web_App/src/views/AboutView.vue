d958e103c75e97cd1bb9e42be58a02ed9e89b1e7
Makan Ajdadi Salekdeh <gusajdama@student.gu.se>
2025-04-30 00:11:25 +0200

    #16 - Add pictures and descriptions with responsiveness to About page

--- Web_App/src/views/AboutView.vue
+++ Web_App/src/views/AboutView.vue
@@ -1,9 +1,68 @@
 @import '../assets/main.css';
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="flex flex-wrap justify-evenly pt-[100px]">
+    
+    <!-- Item 1 -->
+    <div class="flex items-center space-x-2">
+      <img src="/Mikey.png" class="flex w-75 h-75 object-contain rounded-full hover:grayscale transition duration-300" />
+      
+      <div class = "flex flex-col">
+        <h1 class="flex text-xl text-cyan-700 hover:text-cyan-900 hover:text-2xl transition duration-300">Makan Ajdadi</h1>
+        <h2 class="flex text-base text-white hover:text-gray-400 hover:text-lg transition duration-300">SCRUM Master</h2>
+      </div>
+    </div>
+
+    <!-- Item 2 -->
+    <div class="flex items-center space-x-2">
+      <img src="/person_lock_filled_icon_202008.webp" class="flex w-75 h-75 object-contain rounded-full hover:grayscale transition duration-300" />
+      
+      <div class = "flex flex-col">
+        <h1 class="flex text-xl text-cyan-700 hover:text-cyan-900 hover:text-2xl transition duration-300">Sivert Bronebakk</h1>
+        <h2 class="flex text-base text-white hover:text-gray-400 hover:text-lg transition duration-300">Product Owner</h2>
+      </div>
+    </div>
+
+    <!-- Item 3 -->
+    <div class="flex items-center space-x-2">
+      <img src="/person_lock_filled_icon_202008.webp" class="flex w-75 h-75 object-contain rounded-full hover:grayscale transition duration-300" />
+      
+      <div class = "flex flex-col">
+        <h1 class="flex text-xl text-cyan-700 hover:text-cyan-900 hover:text-2xl transition duration-300">Mobin Salehi</h1>
+        <h2 class="flex text-base text-white hover:text-gray-400 hover:text-lg transition duration-300">Developer</h2>
+      </div>
+    </div>
+
+    </div>
+
+  <div class="flex justify-evenly pt-[25px]">
+    <!-- Item 4 -->
+    <div class="flex items-center space-x-2">
+      <img src="/person_lock_filled_icon_202008.webp" class="flex w-75 h-75 object-contain rounded-full hover:grayscale transition duration-300" />
+      
+      <div class = "flex flex-col">
+        <h1 class="flex text-xl text-cyan-700 hover:text-cyan-900 hover:text-2xl transition duration-300">Parham Ashrafzadeh</h1>
+        <h2 class="flex text-base text-white hover:text-gray-400 hover:text-lg transition duration-300">Developer</h2>
+      </div>
+    </div>
+
+    <!-- Item 5 -->
+    <div class="flex items-center space-x-2">
+      <img src="/person_lock_filled_icon_202008.webp" class="flex w-75 h-75 object-contain rounded-full hover:grayscale transition duration-300" />
+      
+      <div class = "flex flex-col">
+        <h1 class="flex text-xl text-cyan-700 hover:text-cyan-900 hover:text-2xl transition duration-300">Dorin Calmis</h1>
+        <h2 class="flex text-base text-white hover:text-gray-400 hover:text-lg transition duration-300">Developer</h2>
+      </div>
+    </div>
+
+
+
   </div>
+
+
+
 </template>
+
 
 <style>
 @media (min-width: 1024px) {
