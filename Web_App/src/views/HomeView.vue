e5e065e13e2ab48fddb265d29d8ed683819f0f3e
Ali Parham Ashrafzadeh <parham.jobb@gmail.com>
2025-05-08 04:45:36 +0200

    #15 - TailwindCSS applied to the 'Home-Page'.

--- Web_App/src/views/HomeView.vue
+++ Web_App/src/views/HomeView.vue
@@ -3,78 +3,107 @@
 
 <template>
   <main>
-    <!--
-    <div class="fixed">
-      <h1>Carduino</h1>
-    </div>
-    -->
-    <div class="container mt-50 mx-auto px-[20vw] box-border text-[#d9d9d9]">
-      <ul class="flex items-stretch my-[2vh] p-0 gap-4 h-[20vh] list-none">
-        <li class="box-border flex-[0_0_25vw]">
-          <img
-            src="/test.png"
-            alt="test"
-            class="block w-full h-full object-contain"
-          />
-        </li>
-        <li class="box-border flex-[0_0_25vw]">
-          <p class="m-0 wrap-break-word">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
-            eligendi explicabo saepe quidem assumenda corrupti culpa recusandae
-            aliquam ut aspernatur, sit nisi! Ullam accusantium dolorum
-            laboriosam explicabo, tenetur reprehenderit fugiat!
-          </p>
-        </li>
-      </ul>
-      <ul class="flex items-stretch my-[2vh] p-0 gap-4 h-[20vh] list-none">
-        <li class="box-border flex-[0_0_25vw]">
-          <img
-            src="/test.png"
-            alt="test"
-            class="block w-full h-full object-contain"
-          />
-        </li>
-        <li class="box-border flex-[0_0_25vw]">
-          <p class="m-0 wrap-break-word">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
-            eligendi explicabo saepe quidem assumenda corrupti culpa recusandae
-            aliquam ut aspernatur, sit nisi! Ullam accusantium dolorum
-            laboriosam explicabo, tenetur reprehenderit fugiat!
-          </p>
-        </li>
-      </ul>
-      <ul class="flex items-stretch my-[2vh] p-0 gap-4 h-[20vh] list-none">
-        <li class="box-border flex-[0_0_25vw]">
-          <img
-            src="/test.png"
-            alt="test"
-            class="block w-full h-full object-contain"
-          />
-        </li>
-        <li class="box-border flex-[0_0_25vw]">
-          <p class="m-0 wrap-break-word">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
-            eligendi explicabo saepe quidem assumenda corrupti culpa recusandae
-            aliquam ut aspernatur, sit nisi! Ullam accusantium dolorum
-            laboriosam explicabo, tenetur reprehenderit fugiat!
-          </p>
-        </li>
-      </ul>
+    <div class="container flex flex-col mt-50 mb-20 mx-auto gap-8">
+      <div>
+        <ul>
+          <li class="flex flex-col lg:flex-row lg:items-start gap-8">
+            <img
+              src="/test.png"
+              alt="test"
+              class="box-border block w-100 h-100 object-contain"
+            />
+
+            <p
+              class="text-[#d9d9d9] container text-justify p-8 max-lg:mb-20 max-lg:-mt-10"
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+              eligendi explicabo saepe quidem assumenda corrupti culpa
+              recusandae aliquam ut aspernatur, sit nisi! Ullam accusantium
+              dolorum laboriosam explicabo, tenetur reprehenderit fugiat! Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Sed eligendi
+              explicabo saepe quidem assumenda corrupti culpa recusandae aliquam
+              ut aspernatur, sit nisi! Ullam accusantium dolorum laboriosam
+              explicabo, tenetur reprehenderit fugiat! Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Sed eligendi explicabo saepe
+              quidem assumenda corrupti culpa recusandae aliquam ut aspernatur,
+              sit nisi! Ullam accusantium dolorum laboriosam explicabo, tenetur
+              reprehenderit fugiat! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Sed eligendi explicabo saepe quidem assumenda
+              corrupti culpa recusandae aliquam ut aspernatur, sit nisi! Ullam
+              accusantium dolorum laboriosam explicabo, tenetur reprehenderit
+              fugiat!
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li class="flex flex-col lg:flex-row lg:items-start gap-8">
+            <img
+              src="/test.png"
+              alt="test"
+              class="box-border block w-100 h-100 object-contain"
+            />
+
+            <p
+              class="text-[#d9d9d9] container text-justify p-8 max-lg:mb-20 max-lg:-mt-10"
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+              eligendi explicabo saepe quidem assumenda corrupti culpa
+              recusandae aliquam ut aspernatur, sit nisi! Ullam accusantium
+              dolorum laboriosam explicabo, tenetur reprehenderit fugiat! Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Sed eligendi
+              explicabo saepe quidem assumenda corrupti culpa recusandae aliquam
+              ut aspernatur, sit nisi! Ullam accusantium dolorum laboriosam
+              explicabo, tenetur reprehenderit fugiat! Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Sed eligendi explicabo saepe
+              quidem assumenda corrupti culpa recusandae aliquam ut aspernatur,
+              sit nisi! Ullam accusantium dolorum laboriosam explicabo, tenetur
+              reprehenderit fugiat! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Sed eligendi explicabo saepe quidem assumenda
+              corrupti culpa recusandae aliquam ut aspernatur, sit nisi! Ullam
+              accusantium dolorum laboriosam explicabo, tenetur reprehenderit
+              fugiat!
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li class="flex flex-col lg:flex-row lg:items-start gap-8">
+            <img
+              src="/test.png"
+              alt="test"
+              class="box-border block w-100 h-100 object-contain"
+            />
+
+            <p
+              class="text-[#d9d9d9] container text-justify p-8 max-lg:mb-20 max-lg:-mt-10"
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+              eligendi explicabo saepe quidem assumenda corrupti culpa
+              recusandae aliquam ut aspernatur, sit nisi! Ullam accusantium
+              dolorum laboriosam explicabo, tenetur reprehenderit fugiat! Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Sed eligendi
+              explicabo saepe quidem assumenda corrupti culpa recusandae aliquam
+              ut aspernatur, sit nisi! Ullam accusantium dolorum laboriosam
+              explicabo, tenetur reprehenderit fugiat! Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Sed eligendi explicabo saepe
+              quidem assumenda corrupti culpa recusandae aliquam ut aspernatur,
+              sit nisi! Ullam accusantium dolorum laboriosam explicabo, tenetur
+              reprehenderit fugiat! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Sed eligendi explicabo saepe quidem assumenda
+              corrupti culpa recusandae aliquam ut aspernatur, sit nisi! Ullam
+              accusantium dolorum laboriosam explicabo, tenetur reprehenderit
+              fugiat!
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 </template>
 
-<style scoped>
-/*
-#name h1 {
-  position: relative;
-  color: rgb(217, 217, 217);
-  text-align: center;
-  margin-top: 10%;
-}
-
-#name:hover {
-  color: #ffcc00;
-}
-*/
-</style>
+<style scoped></style>
